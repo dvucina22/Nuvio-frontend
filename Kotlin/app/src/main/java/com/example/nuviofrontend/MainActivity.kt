68c2bd3e872b02764921d66fb943cb07111f68de
nantolic <nantolic22@student.foi.hr>
2025-11-04 18:55:26 +0100

    Home screen created - setting the main background and logo - colors created that are used on the screen - navigation compose dependency added in build.gradle - added RegistrationScreen

--- Kotlin/app/src/main/java/com/example/nuviofrontend/MainActivity.kt
+++ Kotlin/app/src/main/java/com/example/nuviofrontend/MainActivity.kt
@@ -4,14 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.nuviofrontend.ui.theme.ButtonColorDark
 import com.example.nuviofrontend.ui.theme.NuvioFrontendTheme
+import com.example.nuviofrontend.screens.RegistrationScreen
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +43,80 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NuvioFrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Black
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "main") {
+                        composable("main") { MainScreen(navController) }
+                        composable("registration") { RegistrationScreen() }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NuvioFrontendTheme {
-        Greeting("Android")
+fun MainScreen(navController: NavController){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.background_dark),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 32.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_dark_full),
+                contentDescription = "logo_dark_full",
+                modifier = Modifier.size(230.dp),
+            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = { /*todo login*/ },
+                    modifier = Modifier.fillMaxWidth().height(48.dp).padding(vertical = 4.dp),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = ButtonColorDark)
+                ){
+                    Text("Login", color = Color.White)
+                }
+                Button(
+                    onClick = {
+                        navController.navigate("registration")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(48.dp)
+                        .padding(vertical = 4.dp),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = ButtonColorDark)
+                ) {
+                    Text("Register", color = Color.White)
+                }
+                Text(
+                    text = "Continue as guest",
+                    color = Color(0xFF9DA39F),
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline,
+                    modifier = Modifier.padding(top = 24.dp).clickable {/*todo guest*/}
+                )
+            }
+        }
     }
 }
--- Kotlin/app/src/main/java/com/example/nuviofrontend/ui/theme/Color.kt
+++ Kotlin/app/src/main/java/com/example/nuviofrontend/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.example.nuviofrontend.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val ButtonColorDark = Color(0xFF3E4A48)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
