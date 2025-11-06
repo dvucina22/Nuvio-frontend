eda57d3d2f99226dffadbd6c84a226af78579bd2
nantolic <nantolic22@student.foi.hr>
2025-11-06 18:53:21 +0100

    Fixed app launch issues and navigation setup

--- Kotlin/app/src/main/java/com/example/nuviofrontend/navigation/NavGraph.kt
+++ Kotlin/app/src/main/java/com/example/nuviofrontend/navigation/NavGraph.kt
@@ -4,12 +4,19 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.nuviofrontend.MainScreen
 import com.example.nuviofrontend.feature.auth.presentation.login.LoginScreen
 import com.example.nuviofrontend.feature.auth.presentation.register.RegisterScreen
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Screen.Login.route) {
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(Screen.MainScreen.route) {
+            MainScreen(
+                onNavigateToRegister = { navController.navigate(Screen.Register.route) },
+                onNavigateToLogin = { navController.navigate(Screen.Login.route) }
+            )
+        }
         composable(Screen.Login.route) {
             LoginScreen(
                 onNavigateToRegister = { navController.navigate(Screen.Register.route) },
